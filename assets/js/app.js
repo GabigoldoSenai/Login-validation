693bc77ec4d976b1d84ff6bf09d61abd99e5fd6a
Letícia G. Soares <leticiag.soares.dev@gmail.com>
2023-11-28 17:21:28 -0300

    design fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+const form = document.getElementById('form-box')
 const firstNameInput = document.getElementById('first-name')
 const lastNameInput = document.getElementById('last-name')
 const emailInput = document.getElementById('email')
@@ -5,27 +6,4 @@ const passwordInput = document.getElementById('password')
 const btnSubmit = document.getElementById('btn-submit')
 const inputField = document.querySelector('.input-field')
 
-//desisti disso aqui
-
-btnSubmit.addEventListener('click', () =>{
-    checkName()
-    checkEmail()
-    checkPassword()
-})
-
-function checkName(){
-    if(firstNameInput.value.trim() = ''){
-        alert("a")
-        firstNameInput.classList.add('invalid')
-    }else{
-        firstNameInput.classList.remove('invalid')
-    }
-}
-
-function checkEmail(){
-    alert("b")
-}
-
-function checkPassword(){
-    alert("C")
-}
+btnSubmit
